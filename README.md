d323311167ead5326bf890b8281ea798a3b022f9
Stefan Winkler <stefan@winklerweb.net>
2019-01-03 14:25:54 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # phone-to-timing
 Small script to synchronize Fritz!Box Call List to Timing.app on Mac
+
+# Installation
+
+Install the requirements.
+
+# Configuration
+
+Copy the configuration file `example.phonetotiming.ini` to your home directory and
+rename it to `example.phonetotiming.ini`
+
+Edit the contents:
+
+    [main]
+    password = <your Fritz!Box password here>
+    firstrun = true
+
+For the `password` set the correct Fritz!Box password. This is all you need to do.
+
+The existence of `firstrun` indicates that the script has not run before. On the first run, the script will remove this
+entry and instead add a `lastprocessed` entry which contains the Id of the most recent entry in the call list.
+
+The `lastprocessed` entry will be updated by the script on every run to keep track which calls have been written to 
+Timing.app already.
+
+# Running for the First Time
+
+After you have copied and edited the configuration file, run the script manually for the first time. (No worries! It will not write anything to Timing.app, yet, because the `firstrun` key exists!)
+
+    $ ./phonetotiming.py
+    \o/ - everything seems to work. The latest call on your call list has the Id 1568.
+
+If you see this output, everything is working. The script has read the call list and updated the configuration file with the ID shown.
+Every subsequent phone call will be sent to Timing.app in the next run of the script.
+
+# Running Regularly
+
+To make the script run regularly (default: every 10 minutes), edit the file `net.winklerweb.phonetotiming.plist` and in line 9 set the correct path to the phonetotiming.py script.
+
+    $ cp net.winklerweb.phonetotiming.plist ~/Library/LaunchAgents/
+    $ cd ~/Library/LaunchAgents
+    $ launchctl load net.winklerweb.phonetotiming.plist 
+    $ launchctl start net.winklerweb.phonetotiming.plist 
---
+++ phonetotiming.py
@@ -0,0 +1,117 @@
+#!/opt/local/bin/python
+
+from os.path import expanduser
+from fritzconnection import FritzCall
+from string import Template
+import datetime
+import osascript
+import configparser
+
+# create an AppleScript to execute
+def appleScript(callInfo):
+    template = Template("""
+set dtstart to (current date)
+set day of dtstart to 1
+set year of dtstart to $startyear
+set month of dtstart to $startmonth
+set day of dtstart to $startday
+set hours of dtstart to $starthour
+set minutes of dtstart to $startminute
+set seconds of dtstart to $startsecond
+
+set dtend to (current date)
+set day of dtend to 1
+set year of dtend to $endyear
+set month of dtend to $endmonth
+set day of dtend to $endday
+set hours of dtend to $endhour
+set minutes of dtend to $endminute
+set seconds of dtend to $endsecond
+
+tell application "TimingHelper"
+	add task from ((dtstart)) to ((dtend)) with title "Telefonat mit $caller" project (front project whose name is "Telefon")
+end tell
+""")
+    return template.substitute(callInfo)
+
+# read config file
+cfgname = expanduser("~/.phonetotiming.ini")
+config = configparser.ConfigParser()
+config.read(cfgname)
+fritzPassword = config['main']['password']
+firstRun = 'firstrun' in config['main']
+if firstRun:
+    del config['main']['firstrun']
+    config['main']['lastprocessed'] = '0'
+
+debug = 'debug' in config['main'] and config['main']['debug'] == 'true' 
+if debug:
+    print ("phonetotiming.py running at " + str(datetime.datetime.now()))
+
+lastprocessed = int(config['main']['lastprocessed'])
+currentprocessed = lastprocessed
+
+# connect to Fritz!Box
+fCall = FritzCall(password=fritzPassword)
+
+# read call list
+calls = fCall.get_calls()
+
+for call in calls:
+    callInfo = {}
+    # process all calls with id after lastprocessed
+    if call['Id'] > lastprocessed:
+        if debug:
+            print("Found new Call with Id " + str(call['Id']))
+        currentprocessed = max(currentprocessed, call['Id'])
+
+        if not firstRun:
+            # if there is a phonebook entry for the caller, use that
+            if call['Name'] != None:
+                callInfo['caller'] = call['Name']
+            # else read the remote phone number depending on the type of call
+            else:
+                if call['Type'] == 3:
+                    # outgoing call
+                    callInfo['caller'] = call['Called']
+                else:
+                    # incoming and missed calls
+                    callInfo['caller'] = call['Caller']
+
+            # decompose start and end date
+            startDate = call['Date']
+            callInfo['startyear'] = startDate.year
+            callInfo['startmonth'] = startDate.month
+            callInfo['startday'] = startDate.day
+            callInfo['starthour'] = startDate.hour
+            callInfo['startminute'] = startDate.minute
+            callInfo['startsecond'] = startDate.second
+            
+            endDate = startDate + call['Duration']
+            callInfo['endyear'] = endDate.year
+            callInfo['endmonth'] = endDate.month
+            callInfo['endday'] = endDate.day
+            callInfo['endhour'] = endDate.hour
+            callInfo['endminute'] = endDate.minute
+            callInfo['endsecond'] = endDate.second
+            
+            # fill AppleScript template
+            if debug:
+                print("Executing AppleScript")
+            script = appleScript(callInfo)
+            code,out,err = osascript.run(script)
+            if code != 0:
+                print ("osascript return code = " + str(code))
+                print (out)
+                print (err)
+
+# finally write new lastprocessed into config
+config['main']['lastprocessed'] = str(currentprocessed)
+with open(cfgname, 'w') as configfile:
+    config.write(configfile)
+
+if firstRun:
+    print(r'\o/ - everything seems to work. The latest call on your call list has the Id ' + str(currentprocessed) + '.')
+
+if debug:
+    print("phonetotiming.py done at " + str(datetime.datetime.now()))
